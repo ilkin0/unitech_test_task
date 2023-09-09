8c5a7ac2df7d7a86956d22dd69009cd2f03a5ffd
Ilkin Mehdiyev <hey@ilkinmehdiyev.com>
2023-09-09 14:15:54 +0400

    init commit

---
+++ src/main/java/com/ilkinmehdiyev/unitech_test_task/UnitechTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.ilkinmehdiyev.unitech_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitechTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitechTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilkinmehdiyev/unitech_test_task/UnitechTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilkinmehdiyev.unitech_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitechTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
